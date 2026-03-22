7bfb5d1059a3a21e120cd6062284d3e53b6371fb
PavelSapegin <pawsapegin@yandex.ru>
2026-03-22 20:34:07 +0300

    Formatting

--- src/test_conv.py
+++ src/test_conv.py
@@ -29,64 +29,56 @@ def input_image() -> Image.Image:
 def check_against_golden(result_img: Image.Image, golden_filename: str) -> None:
     """Load golden file and compare with result"""
     golden_path = os.path.join(GOLDEN_DIR, golden_filename)
-    
+
     if not os.path.exists(golden_path):
         pytest.fail(f"Эталонный файл не найден: {golden_path}")
 
     golden_img = Image.open(golden_path).convert("L")
-    
+
     result_arr = np.array(result_img)
     golden_arr = np.array(golden_img)
 
     np.testing.assert_array_equal(
-        result_arr, 
-        golden_arr, 
-        err_msg=f"Результат не совпадает с эталонным файлом {golden_filename}!"
+        result_arr,
+        golden_arr,
+        err_msg=f"Результат не совпадает с эталонным файлом {golden_filename}!",
     )
+
 
 # Parameters (Golden file, kernel, padding)
 KERNELS_TO_TEST = [
+    ("identity_no_padding.png", np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]]), False),
     (
-        "identity_no_padding.png", 
-        np.array([[0, 0, 0], [0, 1, 0],[0, 0, 0]]), 
-        False
+        "edge_detect_padding.png",
+        np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]),
+        True,
     ),
     (
-        "edge_detect_padding.png", 
-        np.array([[-1, -1, -1], [-1,  8, -1],[-1, -1, -1]]), 
-        True
+        "blur_no_padding.png",
+        np.array([[1 / 9, 1 / 9, 1 / 9], [1 / 9, 1 / 9, 1 / 9], [1 / 9, 1 / 9, 1 / 9]]),
+        False,
     ),
-    (
-        "blur_no_padding.png", 
-        np.array([[1/9, 1/9, 1/9], [1/9, 1/9, 1/9],[1/9, 1/9, 1/9]]), 
-        False
-    ),
-    (
-        "sharpen_padding.png", 
-        np.array([[ 0, -1,  0],[-1,  5, -1], [ 0, -1,  0]]), 
-        True
-    ),
+    ("sharpen_padding.png", np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]), True),
 ]
+
 
 @pytest.mark.parametrize("golden_file, kernel, use_padding", KERNELS_TO_TEST)
 def test_conv_with_various_kernels(
-    input_image: Image.Image, 
-    golden_file: str, 
-    kernel: np.ndarray, 
-    use_padding: bool
+    input_image: Image.Image, golden_file: str, kernel: np.ndarray, use_padding: bool
 ) -> None:
     """
     Test function conv on some kernels (blur, sharpen, edge detection)
     comparing result with golden .png files.
     """
     result = conv(input_image, kernel=kernel, padding=use_padding)
-    
+
     check_against_golden(result, golden_file)
+
 
 def test_to_grayscale(input_image: Image.Image) -> None:
     """
     Test grayscale function
     """
-    
+
     result = to_grayscale(input_image)
     check_against_golden(result, "grayscale_expected.png")
